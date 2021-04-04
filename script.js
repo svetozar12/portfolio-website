fae5f40e735ea512933cce648b7d7d1362a8cf0f
svetozar12 <zaauna@abv.bg>
2021-04-04 22:30:18 +0300

    update on the whole desing of the site, as well as the nav bar,the form and the footer

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+//on scroll
+var onAppear = [];//array
 
-var onAppear = [];
-
-document.addEventListener("DOMContentLoaded", function() {
-  onAppear = [].map.call(document.querySelectorAll(".default1"), function(item ) {
-    return item;
-  });
-}, false);
+document.addEventListener("DOMContentLoaded",
+  function () {
+    onAppear = [].map.call(document.querySelectorAll(".default1"), function(item ) {
+      return item;
+    });
+  }, false);
 
 window.addEventListener("scroll", function() {
   onAppear.forEach(function(elem) {
@@ -15,14 +16,13 @@ window.addEventListener("scroll", function() {
     var elemHeight = elem.offsetHeight;
     
        if (vwBottom > elemTop && ((vwTop - elemHeight) < elemTop)) {
-     
-           
-           
+         
              elem.classList.remove("animation2_fade");
              elem.classList.add("default1");
           }  
 
        else {
+
 
         elem.classList.add("animation2_fade");
         elem.classList.remove("default1");
@@ -30,5 +30,20 @@ window.addEventListener("scroll", function() {
     });
   }, 
   false);
+//burger menu 
+
+    function myFunction(x) {
+      x.classList.toggle("change");
+    }
+		function openNav()  {
+			document.getElementById("burger-menu").classList.toggle("open");
+	  }    
+		function closeNav() {
+			document.getElementById("burger-menu").style.widht="0px";
+    }
+
+    //rewriting in css in javascript
+
+
 
 
