f08c41a2b571b715455d1318503df1fdfc03a867
svetozar12 <zaauna@abv.bg>
2021-04-24 00:57:32 +0300

    fixed the problem with css but now js is making problems, will fix them tommorow

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ window.addEventListener("load", () => {
     let red = document.getElementById("red");
     let green = document.getElementById("green");
     let blue = document.getElementById("blue");
+    let code = document.getElementsByClassName("red");
     //() => {
         let project1 = document.getElementById("first");
         let project2 = document.getElementById("second");
@@ -11,12 +12,13 @@ window.addEventListener("load", () => {
         let project5 = document.getElementById("sub-second");
         let project6 = document.getElementById("sub-third");
     box.addEventListener("mouseover", () => {
-        red.classList.add("hover-class");
+       /* red.classList.add("hover-class");
         green.classList.add("hover-class");
-        blue.classList.add("hover-class");
+        blue.classList.add("hover-class");*/
         project1.classList.add("opacity-p");
         project2.classList.add("opacity-p");
         project3.classList.add("opacity-p");
+        //code[0].classList.remove("opacity-p");//have to fix this
         project4.classList.add("sub-opacity-p");
         project5.classList.add("sub-opacity-p");
         project6.classList.add("sub-opacity-p");
@@ -24,12 +26,13 @@ window.addEventListener("load", () => {
         });
     
     box.addEventListener("mouseout", () => {
-        red.classList.remove("hover-class");
+        /*red.classList.remove("hover-class");
         green.classList.remove("hover-class");
-        blue.classList.remove("hover-class");
+        blue.classList.remove("hover-class");*/
         project1.classList.remove("opacity-p");
         project2.classList.remove("opacity-p");
         project3.classList.remove("opacity-p");
+        //code[0].classList.add("opacity-p");//this as well
         project4.classList.remove("sub-opacity-p");
         project5.classList.remove("sub-opacity-p");
         project6.classList.remove("sub-opacity-p");
